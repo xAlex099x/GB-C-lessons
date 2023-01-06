9573bd5aaea128948c4a5a3a9137bc0c20887e60
xAlex099x <Alex09920000@gmail.com>
2023-01-06 18:26:45 +0400

    добавил решение 2 задачи

---
+++ lection001/Exp002/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 10;
+int b = 20;
+int c = 1;
+
+
+Console.WriteLine("Числа: " + a + ", " + b + ", " + c);
+
+if (a > b)
+{
+    if(a > c)
+    {
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+else
+{
+    if(b > c)
+    {
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
