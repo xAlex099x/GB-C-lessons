4f74dff63dbbe4b51f2d947ad224c36ff8aec0be
xAlex099x <Alex09920000@gmail.com>
2023-01-07 14:53:03 +0400

    добавил решение и блок схему 3 задачи

---
+++ lection002/Exp003/Program.cs
@@ -0,0 +1,15 @@
+﻿void itsHoliday(int number)
+{
+    if(number == 6 && number == 7)
+    {
+        Console.WriteLine("Это выходной");
+    }
+    else
+    {
+        Console.WriteLine("Это будний день");
+    }
+}
+
+int number = 3;
+
+itsHoliday(number);
