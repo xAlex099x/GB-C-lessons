f61a56001c8110ab50f0e211b66cc522ffcf018a
xAlex099x <Alex09920000@gmail.com>
2023-01-07 14:45:08 +0400

    добавил решение и блок схему 2 задачи

---
+++ lection002/Exp002/Program.cs
@@ -0,0 +1,16 @@
+﻿void thirdDigit (int number)
+{
+    string s = number.ToString();
+    if (s.Length < 3)
+    {
+        Console.WriteLine("Третьей цифры нету");
+    }
+
+    else
+    {
+        Console.WriteLine(s[2]);
+    }
+}
+
+int number = 1834912;
+thirdDigit(number);
