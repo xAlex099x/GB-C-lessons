e402d387d7174a2bdc422db4531549698219fc3a
xAlex099x <Alex09920000@gmail.com>
2023-01-08 20:11:14 +0400

    добавлен новый каталог и решение задачи 0

---
+++ Non_lection/Exp000/Program.cs
@@ -0,0 +1,18 @@
+﻿//1 вариант
+int digit = 3;
+int pow = 5;
+
+
+int index = 1;
+int sum = digit;
+while(index < pow)
+{
+    sum = sum * digit;
+    index++;
+}
+
+
+
+//2 вариант, через встроенный функционал.
+
+Console.WriteLine(Math. Pow(digit, pow));
