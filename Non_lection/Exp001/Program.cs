e8796cae4b749be8f4722c46447ff86207ba5186
xAlex099x <Alex09920000@gmail.com>
2023-01-08 20:23:02 +0400

    добавил решение 1 задачи

---
+++ Non_lection/Exp001/Program.cs
@@ -0,0 +1,11 @@
+﻿int first_digit = 5;
+int second_digit = 26;
+
+if(first_digit * first_digit == second_digit)
+{
+    Console.WriteLine("Второе число это квадрат первого");
+}
+else
+{
+    Console.WriteLine("Второе число не квадрат первого");
+}
