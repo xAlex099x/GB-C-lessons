5ad562e78d0ea1d513d91fbfdf40f5198077912e
xAlex099x <Alex09920000@gmail.com>
2023-01-08 21:15:11 +0400

    добавил решение 3 задачи

---
+++ Non_lection/Exp002/Program.cs
@@ -0,0 +1,29 @@
+﻿int number = 2;
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Некорректное число, введите число от 1 до 7");
+        break;
+}
