22915673765e23e4ef1bbefd57454034f08ac622
xAlex099x <Alex09920000@gmail.com>
2023-01-09 09:54:00 +0400

    добавил решение 12 задачи

---
+++ Non_lection/Exp006/Program.cs
@@ -0,0 +1,10 @@
+﻿Random rnd = new Random(); int number = rnd.Next(100, 999);
+string str = number.ToString();
+
+
+int newInt = int.Parse(str.Substring(0, 1) + str.Substring(2, 1)); // способ через подстроку
+string newStr = str[0] + str[2].ToString(); //способ через обращение к символу
+
+Console.WriteLine("Изначальное число: " + str);
+Console.WriteLine("Новое число через подстроку: " + newInt);
+Console.WriteLine("Новое число через символ: " + newStr);
