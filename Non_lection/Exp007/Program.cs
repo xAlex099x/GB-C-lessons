ef0c231713835f30550a503920fea75d1608b79b
xAlex099x <Alex09920000@gmail.com>
2023-01-09 11:11:57 +0400

    добавил решение 15 задачи

---
+++ Non_lection/Exp007/Program.cs
@@ -0,0 +1,20 @@
+﻿Random rnd = new Random();
+int number = rnd.Next(1,100);
+
+if(number % 7 == 0)
+{
+    Console.WriteLine("Число " + number + " кратно 7");
+}
+else
+{
+    Console.WriteLine("Число " + number + " не кратно 7");
+}
+
+if(number % 23 == 0)
+{
+    Console.WriteLine("Число " + number + " кратно 23");
+}
+else
+{
+    Console.WriteLine("Число " + number + " не кратно 23");
+}
