b478859245ebb79bb55395eebad5b51df5af91da
xAlex099x <Alex09920000@gmail.com>
2023-01-09 09:38:47 +0400

    добавил решение 11 задачи

--- Non_lection/Exp004/Program.cs
+++ Non_lection/Exp004/Program.cs
@@ -1,9 +1,9 @@
 ﻿int number = 967;
 string str = number.ToString();
 
-if(str.Length > 2)
+if (str.Length > 2)
 {
-Console.WriteLine(number % 10 ); // Способ через остаток от деления
-Console.WriteLine(str[2]); // Через конвертацию в строку и обращению к символу
-Console.WriteLine(str.Substring(2,1)); //Через подстроку
+    Console.WriteLine(number % 10); // Способ через остаток от деления
+    Console.WriteLine(str[2]); // Через конвертацию в строку и обращению к символу
+    Console.WriteLine(str.Substring(2, 1)); //Через подстроку
 }
---
+++ Non_lection/Exp005/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rnd = new Random();
+int number = rnd.Next(10, 99);
+string str = number.ToString();
+
+if (int.Parse(str.Substring(0, 1)) > int.Parse(str.Substring(1, 1)))
+{
+    Console.WriteLine("Число " + number + ": первая цифра больше");
+}
+else
+{
+    Console.WriteLine("Число " + number + ": вторая цифра больше");
+}
