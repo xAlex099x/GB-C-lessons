8b9831a5a5a6e46c839ed91c70437437f7fbf8e8
xAlex099x <Alex09920000@gmail.com>
2023-01-11 19:27:35 +0400

    поправлены названия переменных в методе

--- lection004/Exp001/Program.cs
+++ lection004/Exp001/Program.cs
@@ -2,16 +2,16 @@
 {
     int absNumber = Math.Abs(number);
     string tempStr = System.Convert.ToString(absNumber);
-    int res = 0;
+    int result = 0;
 
     for (int i = 0; i <= tempStr.Length - 1; i++)
     {
-        res += (int)Char.GetNumericValue(tempStr[i]);
+        result += (int)Char.GetNumericValue(tempStr[i]);
     }
-    return res;
+    return result;
 }
 
 Random rnd = new Random();
-int number = rnd.Next(100,100000);
+int number = rnd.Next(0,100000);
 Console.WriteLine($"Число: {number}");
 Console.WriteLine($"Сумма: {digitSum(number)}");
