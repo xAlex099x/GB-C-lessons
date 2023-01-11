83a526aeadabf7fed9aa91509f02991a15d2d156
xAlex099x <Alex09920000@gmail.com>
2023-01-11 23:42:48 +0400

    добавил решение 28 задачи, поправил ссылку на 27 задачу

---
+++ Non_lection/Exp008/Program.cs
@@ -0,0 +1,23 @@
+﻿int digitCount(int number)
+{
+    int absNumber = Math.Abs(number);
+    int tempNumber = absNumber;
+    int count = 0;
+
+    while (tempNumber > 0)
+    {
+        tempNumber = (int)(tempNumber / 10);
+        count++;
+    }
+
+    return count;
+}
+
+Random rnd = new Random();
+int number = rnd.Next(10, 1000);
+
+int result = digitCount(number);
+Console.WriteLine(number);
+Console.WriteLine(result);
+
+
