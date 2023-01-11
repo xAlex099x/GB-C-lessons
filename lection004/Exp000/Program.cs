53e8c73d572c98f6ed5daad34c4a5353dc875ee5
xAlex099x <Alex09920000@gmail.com>
2023-01-11 19:22:17 +0400

    добавлен каталог 4 лекции и решения задач

---
+++ lection004/Exp000/Program.cs
@@ -0,0 +1,46 @@
+﻿
+int[] newRndBinary(int size = 10)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+
+    array[0] = 1;
+    return array;
+}
+
+int binaryToDecimal(int[] binaryArray)
+{
+    int result = 0;
+    int temp = binaryArray.Length - 1;
+    for (int i = 0; i <= binaryArray.Length - 1; i++, temp--)
+    {
+        if (binaryArray[i] != 0 && binaryArray[i] != 1)
+        {
+            return -1;
+        }
+        result += binaryArray[i] * (int)Math.Pow(2, temp);
+    }
+    return result;
+}
+
+void printBinary(int[] array, int result)
+{
+    Console.Write("Массив: ");
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine($" -> Результат: {result}");
+}
+
+
+int[] testArray = newRndBinary(7);
+int number = binaryToDecimal(testArray);
+
+printBinary(testArray, number);
+
---
+++ lection004/Exp001/Program.cs
@@ -0,0 +1,17 @@
+﻿int digitSum(int number)
+{
+    int absNumber = Math.Abs(number);
+    string tempStr = System.Convert.ToString(absNumber);
+    int res = 0;
+
+    for (int i = 0; i <= tempStr.Length - 1; i++)
+    {
+        res += (int)Char.GetNumericValue(tempStr[i]);
+    }
+    return res;
+}
+
+Random rnd = new Random();
+int number = rnd.Next(100,100000);
+Console.WriteLine($"Число: {number}");
+Console.WriteLine($"Сумма: {digitSum(number)}");
