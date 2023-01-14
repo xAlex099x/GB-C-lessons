2e5b583f2bc9603992bb182a36d865e6ad387616
xAlex099x <Alex09920000@gmail.com>
2023-01-14 17:03:13 +0400

    добавлена лекция 6, задачи 41 и 43

---
+++ lection006/Exp000/Program.cs
@@ -0,0 +1,54 @@
+﻿string[] UserInput()
+{
+    Console.WriteLine("Введите числа через запятую: ");
+    string strInput = Console.ReadLine();
+    strInput = strInput.Replace(" ", "");
+    string[] input = strInput.Split(",");
+    return input;
+}
+int[] StrArrayToInt(string[] strArray)
+{
+    int[] intArray = new int[strArray.Length];
+
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        intArray[i] = int.Parse(strArray[i]);
+    }
+
+    return intArray;
+}
+int numberGreaterZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void PrintArray(int[] intArray)
+{
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        Console.Write($"{intArray[i]};");
+    }
+}
+
+while (true)
+{
+    string[] strArrInput = UserInput();
+
+    int[] intArray = StrArrayToInt(strArrInput);
+
+    int count = numberGreaterZero(intArray);
+
+    Console.ForegroundColor = ConsoleColor.Blue;
+    PrintArray(intArray);
+    Console.WriteLine("\nЧисел больше нуля: " + count);
+    Console.ResetColor();
+    
+    break;
+}
---
+++ lection006/Exp001/Program.cs
@@ -0,0 +1,42 @@
+﻿while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+
+    Console.WriteLine("Введите b1: ");
+    bool res = int.TryParse(Console.ReadLine(), out int b1);
+    if (res == false)
+    {
+        Console.WriteLine("Недопустимый ввод.");
+        break;
+    }
+
+    Console.WriteLine("Введите k1: ");
+    bool res2 = int.TryParse(Console.ReadLine(), out int k1);
+    if (res2 == false)
+    {
+        Console.WriteLine("Недопустимый ввод.");
+        break;
+    }
+
+    Console.WriteLine("Введите b2: ");
+    bool res3 = int.TryParse(Console.ReadLine(), out int b2);
+    if (res3 == false)
+    {
+        Console.WriteLine("Недопустимый ввод.");
+        break;
+    }
+
+    Console.WriteLine("Введите k2: ");
+    bool res4 = int.TryParse(Console.ReadLine(), out int k2);
+    if (res4 == false)
+    {
+        Console.WriteLine("Недопустимый ввод.");
+        break;
+    }
+
+    Console.WriteLine($"x = {(double)(b2 - b1) / (k1 - k2)}");
+    Console.WriteLine($"y = {(double)((b2 + k1) - (b1 + k2)) / (k2 - k1)}");
+    
+    Console.ResetColor();
+    break;
+}
