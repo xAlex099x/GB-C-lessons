88fb9faeb0b50074bf71953ecc9e430685a20692
xAlex099x <Alex09920000@gmail.com>
2023-01-24 10:14:09 +0400

    добавлены решения задач для 9 лекции

---
+++ lection009/Exp002/Program.cs
@@ -0,0 +1,15 @@
+﻿int UserInput()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+double AkkermanFunction(double m, double n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return AkkermanFunction(m - 1, 1);
+    else return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
+
+var m = UserInput();
+var n = UserInput();
+Console.WriteLine($"A({m},{n}) = {AkkermanFunction(m, n)}");
